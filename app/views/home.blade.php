3aa8c55f84ea9b47a23a96c424431adf27462220
marktristan <mt.victorio@dot.ph>
2013-12-12 16:23:44 +0800

    Added blade template

---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+<CODE>HELLO WORLD!</CODE>
+@stop
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8" />
+		<title>
+			@section('title')
+				Laravel 4 - Bootstrap App
+			@show
+		</title>
+		<meta name="keywords" content="your, awesome, keywords, here" />
+		<meta name="author" content="Jon Doe" />
+		<meta name="description" content="Lorem ipsum dolor sit amet, nihil fabulas et sea, nam posse menandri scripserit no, mei." />
+
+		<!-- Mobile Specific Metas
+		================================================== -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- CSS
+		================================================== -->
+		<link href="{{{ asset('assets/css/bootstrap.css') }}}" rel="stylesheet">
+
+		<style>
+			@section('styles')
+				body {
+					padding-top: 20px;
+				}
+			@show
+		</style>
+
+		<link href="{{{ asset('assets/css/bootstrap-responsive.css') }}}" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		@yield('content')
+	</body>
+</html>
